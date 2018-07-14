1223cb23a08aed370d2f80954750140007ccd1fc
xuzikuan12 <xuzikuan12@qq.com>
2018-07-14 21:20:36 +0800

    更改实现逻辑
    代码模块化，优化打印进度信息

--- python3_main.py
+++ python3_main.py
@@ -14,90 +14,75 @@ def mk_artists(parrent):
     glue = ' ft. '
     return glue.join(artists_arr)
 
-def get_song_info(songid):
+def save_file(var, url, file_name):
+    if os.path.isfile(file_name):
+        print('%s already existed.' % var)
+    else:
+        print('downloading...%s' % var)
+        url_content = requests.get(url).content 
+        with open(file_name, 'wb') as f:
+            f.write(url_content)
+            print('%s saved' % var)
+
+def download_songid(songid, target_dir):
     url = 'http://music.163.com/api/song/detail/?id=' + songid + '&ids=[' + songid +']'
     res = get_json(url)
     songs = res['songs'][0]
-    return {
-        'name' : songs['name'],
-        'id' : songs['id'],
-        'artists' : mk_artists(songs)
+    
+    song = {
+            'name' : songs['name'].replace('/', ''),
+            'songid' : songid,
+            'artists' : mk_artists(songs)
         }
-
-def get_playlist_info(playlist):
-    url = 'http://music.163.com/api/playlist/detail?id=' + playlist
-    return get_json(url)
-
-def get_songs_arr_from_playlist(playlist_json):
-    tracks_arr = playlist_json['result']['tracks']
-    songs_arr = []
-    for track in tracks_arr:
-        each_song = {
-            'name' : track['name'],
-            'id' : track['id'],
-            'artists' : mk_artists(track)
-            }
-        songs_arr.append(each_song)
-    return songs_arr
+    download_song(song, target_dir)
 
 def download_song(song, target_dir):
-    print("\nDownloading... songid: %s, name: %s, artists: %s" % (song['id'], song['name'], song['artists']))
-    name = song['name']
-    name = name.replace('/', '')
-    songid = str(song['id'])
-    artists = song['artists']
-    file_name = "%s\\%s-%s.mp3" % (target_dir, name, artists)
-    if os.path.isfile(file_name):
-        print('File already existed.')
-        return 0
-    song_outer_url = 'http://music.163.com/song/media/outer/url?id=' + songid
-    headers = requests.head(song_outer_url).headers
-    if 'Content-Length' in headers:
-        print('Found songlink: %s' % headers['Location'])
-    else:
-        print('Songlink not found')
-        return 0
-    song_mp3 = requests.get(song_outer_url).content   
-    with open(file_name, 'wb') as song_file:
-        song_file.write(song_mp3)
-        print('Mp3 file saved')
+    print("\nSongid: %s, name: %s, artists: %s" % (song['songid'], song['name'], song['artists']))
+    song_url = 'http://music.163.com/song/media/outer/url?id=' + song['songid']
+    file_name = "%s\\%s-%s.mp3" % (target_dir, song['name'], song['artists'])
+    save_file('Music', song_url, file_name)
 
-def get_mv_info(mv_id):
-    url = 'http://music.163.com/api/mv/detail?id=%s' % mv_id
-    return get_json(url)
+def download_playlist(playlist, target_dir):
+    print("Get msg from playlist: %s" % playlist)
+    url = 'http://music.163.com/api/playlist/detail?id=' + playlist
+    playlist_json = get_json(url)
+    if playlist_json == None:
+        print("Get songlist contents failed")
+        exit()
 
-def download_mv(mv_json, target_dir, clarity):
-    
+    tracks_arr = playlist_json['result']['tracks']
+    for track in tracks_arr:
+        song = {
+            'name' : track['name'].replace('/', ''),
+            'songid' : str(track['id']),
+            'artists' : mk_artists(track)
+            }
+        download_song(song, target_dir)
+
+def download_mv(mv_id, target_dir, clarity):
     clarities = ['240', '480', '720', '1080']
     if clarity not in clarities:
         clarity = 1080
         print('automate select, clarity = 1080')
+        
+    url = 'http://music.163.com/api/mv/detail?id=%s' % mv_id
+    mv_json = get_json(url)
+    if mv_json == None:
+        print("Get mv contents failed")
+        exit()
+    
     data = mv_json['data']
     name = data['name']
     artists = mk_artists(data)
-    print("\nDownloading... mv id: %s, name: %s, artists: %s" % (mv_json['data']['id'], name, artists))
+
+    print("\nMV id: %s, name: %s, artists: %s" % (mv_id, name, artists))
     cover_url = data['cover']
     cover = "%s\\%s-%s.jpg" % (target_dir, name, artists)
-    if os.path.isfile(cover):
-        print('Cover already existed.')
-    else:
-        print('downloading...')
-        cover_content = requests.get(cover_url).content 
-        with open(cover, 'wb') as cover_file:
-            cover_file.write(cover_content)
-            print('Cover saved')
+    save_file('Cover', cover_url, cover)
     
     video_url = data['brs'][clarity]
     video = "%s\\%s-%s.mp4" % (target_dir, name, artists)
-    if os.path.isfile(video):
-        print('Video already existed.')
-    else:
-        print('downloading...')
-        video_content = requests.get(video_url).content 
-        with open(video, 'wb') as video_file:
-            video_file.write(video_content)
-            print('Video saved')
-    
+    save_file('Video', video_url, video)   
     
 if __name__ == '__main__':
     if len(sys.argv) < 4:
@@ -105,35 +90,20 @@ if __name__ == '__main__':
     mode = sys.argv[1]
     target_dir = sys.argv[2]
     if not os.path.exists(target_dir):
-        os.makedirs(target_dir) 
+        os.makedirs(target_dir)
+        
     if mode == 'song':
         songid = str(sys.argv[3])
-        song = get_song_info(songid)
-        download_song(song, target_dir)
+        download_songid(songid, target_dir)
     elif mode == 'playlist':
         playlist = sys.argv[3]
-        print("Get msg from playlist: %s" % playlist)
-        playlist_json = get_playlist_info(playlist)
-        if playlist_json == None:
-            print("Get songlist contents failed")
-            exit()
-        if not os.path.exists(target_dir):
-            os.makedirs(target_dir) 
-        songs_arr = get_songs_arr_from_playlist(playlist_json)
-        for song in songs_arr:
-            download_song(song, target_dir)
+        download_playlist(playlist, target_dir)
     elif mode == 'mv':
         if len(sys.argv) < 5:
             exit('Not enough arguments')
         mv_id = str(sys.argv[3])
-        mv_json = get_mv_info(mv_id)
-        if mv_json == None:
-            print("Get mv contents failed")
-            exit()
-        if not os.path.exists(target_dir):
-            os.makedirs(target_dir)
         clarity = sys.argv[4]
-        download_mv(mv_json, target_dir, clarity)
+        download_mv(mv_id, target_dir, clarity)
     elif mode == 'test':
         print('haha, No IndentationError')
     
--- run.cmd
+++ run.cmd
@@ -1,4 +1,4 @@
-python python3_main.py song D:\xuzikuan12\music\  27965273
+python python3_main.py song D:\xuzikuan12\music\  27965271
 python python3_main.py playlist D:\xuzikuan12\music\ 2313767472
-python python3_main.py mv D:\xuzikuan12\music\mv 5402212  240
-python python3_main.py test D:\xuzikuan12\music\mv 1
+python python3_main.py mv D:\xuzikuan12\music\mv 5402213  240
+python python3_main.py test D:\xuzikuan12\music\mv .
